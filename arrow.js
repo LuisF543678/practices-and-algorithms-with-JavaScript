94798759554db6b94346c8a0e907ab95e342c4f2
Luisf543678 <luisf543678@gmail.com>
2021-05-12 12:25:54 -0500

    new class js

---
+++ arrow.js
@@ -0,0 +1,13 @@
+/* function rSquare(x) {
+    return x * x;
+}
+
+const square = function(x){
+return x * x; 
+} */
+
+//const square = (x) => x * x;
+
+const square = (x) => { return x * x };
+
+console.log(square(2));

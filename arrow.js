13eb101855ad2a67d3ac7a3853daa6a58fbc9987
Luisf543678 <luisf543678@gmail.com>
2021-05-13 11:30:31 -0500

    upgrade

--- arrow.js
+++ arrow.js
@@ -1,12 +1,30 @@
-/* function rSquare(x) {
+
+// Funcion normal 
+
+function rSquare(x) {
     return x * x;
 }
 
-const square = function(x){
-return x * x; 
-} */
+console.log(rSquare(2));
 
-//const square = (x) => x * x;
+
+// Funcion de tipo const sin flecha
+
+const square = function (x) {
+    return x * x;
+}
+
+console.log(square(2));
+
+
+// Funcion tipo flecha con r implicito
+
+const square = (x) => x * x;
+
+console.log(square(2));
+
+
+// Funcion flecha con return 
 
 const square = (x) => { return x * x };
 

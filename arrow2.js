54081c3e999dabad58f974df355b2c3483a2556f
Luisf543678 <luisf543678@gmail.com>
2021-05-14 16:20:19 -0500

    new file

---
+++ arrow2.js
@@ -0,0 +1,27 @@
+
+// fat arrow function
+// funciones anonimas
+
+/* function suma (a,b) {
+    return a+ b;
+}
+
+const resultado = suma(1, 5)
+
+console.log(resultado); */
+
+// tiene return implicito fat
+//const suma = (a,b) => a+b
+/* const suma = (a,b) =>  {
+    const c = a+b
+    return c+1
+} */
+
+// sintaxis antes
+
+const suma = (a,b) => a+b
+
+
+const resultado = suma(1,5);
+
+console.log(resultado);

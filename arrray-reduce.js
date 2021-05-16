f701d848eeadf1703fb4eee5b2d9b757e84073ea
Luisf543678 <luisf543678@gmail.com>
2021-05-16 18:48:46 -0500

    modifiying

--- arrray-reduce.js
+++ arrray-reduce.js
@@ -1,16 +1,20 @@
 
-// funcion reducer
+// funcion reducer-1
 
-//const reducer = (acumulador, valorActual) => nuevoAcumulador;
+const reducer = (acumulador, valorActual) => nuevoAcumulador;
 
-//const reducido = [1, 2].reduce((acc, el) => acc + el, 0);
+const reducido = [1, 2].reduce((acc, el) => acc + el, 0);
 
-//console.log(reducido);
+console.log(reducido);
 
+
+// funcion con reduce-2
 const numeros = [1, 2, 3, 4, 5];
 const resultado = numeros.reduce((acc, el) => acc + el, 0);
-//console.log(resultado);
+console.log(resultado);
 
+
+// Funcion reduce-3
 const mascotas = [
     { nombre: 'pelusa', edad: 12, tipo: 'gato' },
     { nombre: 'puchini', edad: 12, tipo: 'perro' },
@@ -23,8 +27,10 @@ const indexed = mascotas.reduce((acc,el) => ({
     [el.nombre]:el,
 }),{})
 
-//console.log(indexed['pelusa']);
+console.log(indexed['pelusa']);
 
+
+// funcion reduce-4
 const anidado = [1,[2,3],4,[5]];
 //[1,2,3,4,5]
 const plano = anidado.reduce((acc,el) => acc.concat(el),[]);

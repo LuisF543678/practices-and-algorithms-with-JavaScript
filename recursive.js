1d0203b7b8e579fa55e94f4cace3f083687a2d51
Luisf543678 <luisf543678@gmail.com>
2021-06-01 19:03:26 -0500

    adding recursion

---
+++ recursive.js
@@ -0,0 +1,31 @@
+/*
+const conteoRegresivo = (a) => {
+    if (a < 0) {
+        console.log(a)
+        return conteoRegresivo(a - 1)
+    }
+}
+
+conteoRegresivo(10)
+
+*/
+
+// version 1
+const axios = require('axios')
+
+const llamdaApi = async (url, llamados = 0) => {
+    try {
+        const result = await axios.get(url)
+        console.log(result)
+        return result
+    } catch (e) {
+        if (llamados > 5) {
+            return ''
+        }
+        console.log(e)
+        return llamdaApi(url, llamados + 1)
+    }
+}
+
+llamdaApi('https://jsonplaceholder.typicode.com/users/')
+

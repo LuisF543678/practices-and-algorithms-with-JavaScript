cd6e62a77201f9beb3e57a2424ec493394697507
Luisf543678 <luisf543678@gmail.com>
2021-07-24 23:29:41 -0500

    documenting code 1-1

--- fibonacci.js
+++ fibonacci.js
@@ -1,12 +1,27 @@
 
+
+/**
+ * Solcitar el numero para la serie
+ */
 const num = parseInt(
   prompt('Enter the number: ')
 );
 
+/**
+ * Variables para la serie
+ */
 let nm1 = 0, nm2 = 1, nextNum;
 
+
+/**
+ * Imprimir titulo
+ */
 console.log('Fibonacci Serie:');
 
+
+/**
+ * Recorrear y imprimir la serie
+ */
 for (let i = 1; i <= num; i++) {
   console.log(nm1);
   nextNum = nm1 + nm2;

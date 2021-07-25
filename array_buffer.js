7834ce14b5ce0b936cceb1a3bf54b5308e110fa2
Luisf543678 <luisf543678@gmail.com>
2021-07-25 00:14:22 -0500

    documenting code 1-2

--- array_buffer.js
+++ array_buffer.js
@@ -1,2 +1,10 @@
-let buffer = new ArrayBuffer(16); 
+
+/**
+ * Crear un buffer con la logintud de 16
+ */
+let buffer = new ArrayBuffer(16);
+
+/**
+ * Imprimir en consola
+ */
 console.log(buffer.byteLength);
